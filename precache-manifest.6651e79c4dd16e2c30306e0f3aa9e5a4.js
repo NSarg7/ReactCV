4d00b605bfd56dace2ea9b8a4e5d78e88eedd59a
NSarg7 <NSarg7@users.noreply.github.com>
2019-11-17 15:31:43 +0400

    Updates

---
+++ precache-manifest.6651e79c4dd16e2c30306e0f3aa9e5a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fdce70d20bfdb4321d06e0b58136f8",
+    "url": "/ReactCV/index.html"
+  },
+  {
+    "revision": "0e1b77329652ede7889f",
+    "url": "/ReactCV/static/css/main.1b4d0c17.chunk.css"
+  },
+  {
+    "revision": "5dc513bb5c5dd6eab1d5",
+    "url": "/ReactCV/static/js/2.8d1aeece.chunk.js"
+  },
+  {
+    "revision": "0e1b77329652ede7889f",
+    "url": "/ReactCV/static/js/main.aec47183.chunk.js"
+  },
+  {
+    "revision": "6629d28d603eaa4c8081",
+    "url": "/ReactCV/static/js/runtime-main.5227be6d.js"
+  },
+  {
+    "revision": "9c77535137064732be3d22f946b7223f",
+    "url": "/ReactCV/static/media/back.9c775351.jpg"
+  },
+  {
+    "revision": "8e8064118de04ff0ea2b7a47f03b6a03",
+    "url": "/ReactCV/static/media/budgety.8e806411.png"
+  },
+  {
+    "revision": "62459fb8c067b595b751d6146f494ec2",
+    "url": "/ReactCV/static/media/favicon.62459fb8.png"
+  },
+  {
+    "revision": "3ac8e9460900484577105e0fa052205a",
+    "url": "/ReactCV/static/media/forkify.3ac8e946.png"
+  },
+  {
+    "revision": "a797f42a548a99143e9af3f57d5a198e",
+    "url": "/ReactCV/static/media/natours.a797f42a.png"
+  },
+  {
+    "revision": "e5d3fa39d5c461ae9eb2eca78f0672d3",
+    "url": "/ReactCV/static/media/omnifood.e5d3fa39.png"
+  },
+  {
+    "revision": "dcf8448033a1480de4636b9e8b6f4c0e",
+    "url": "/ReactCV/static/media/pig game.dcf84480.png"
+  }
+]);

23e9c6423e6e2585007d9b65bdc51ae456f38c65
NSarg7 <NSarg7@users.noreply.github.com>
2019-11-17 17:07:36 +0400

    Updates

---
+++ precache-manifest.be33c8636bed69aecf3f36c05e841e4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b765ad3f0f645bcb61aa5d558aec5f3",
+    "url": "/ReactCV/index.html"
+  },
+  {
+    "revision": "690012f55db3388950c5",
+    "url": "/ReactCV/static/css/main.ae11e72e.chunk.css"
+  },
+  {
+    "revision": "5dc513bb5c5dd6eab1d5",
+    "url": "/ReactCV/static/js/2.8d1aeece.chunk.js"
+  },
+  {
+    "revision": "690012f55db3388950c5",
+    "url": "/ReactCV/static/js/main.12f33556.chunk.js"
+  },
+  {
+    "revision": "6629d28d603eaa4c8081",
+    "url": "/ReactCV/static/js/runtime-main.5227be6d.js"
+  },
+  {
+    "revision": "b704745a63e39bf7d794647ebdbca530",
+    "url": "/ReactCV/static/media/back.b704745a.jpg"
+  },
+  {
+    "revision": "8e8064118de04ff0ea2b7a47f03b6a03",
+    "url": "/ReactCV/static/media/budgety.8e806411.png"
+  },
+  {
+    "revision": "62459fb8c067b595b751d6146f494ec2",
+    "url": "/ReactCV/static/media/favicon.62459fb8.png"
+  },
+  {
+    "revision": "3ac8e9460900484577105e0fa052205a",
+    "url": "/ReactCV/static/media/forkify.3ac8e946.png"
+  },
+  {
+    "revision": "a797f42a548a99143e9af3f57d5a198e",
+    "url": "/ReactCV/static/media/natours.a797f42a.png"
+  },
+  {
+    "revision": "e5d3fa39d5c461ae9eb2eca78f0672d3",
+    "url": "/ReactCV/static/media/omnifood.e5d3fa39.png"
+  },
+  {
+    "revision": "dcf8448033a1480de4636b9e8b6f4c0e",
+    "url": "/ReactCV/static/media/pig game.dcf84480.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9877ea8d6d7039c0eacbdd9c68f27c0.js"
+  "/ReactCV/precache-manifest.be33c8636bed69aecf3f36c05e841e4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactCV/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
